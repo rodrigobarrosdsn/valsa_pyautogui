94863c13013246fb47a2fbaae4ff5d28a0a72829
rodrigodsn <rodrigo.barros.dsn@gmail.com>
2023-10-09 17:29:53 -0300

    feat: update

--- valsa_2/app/main.py
+++ valsa_2/app/main.py
@@ -1,23 +1,48 @@
 import pyautogui
-screenWidth, screenHeight = pyautogui.size() # Get the size of the primary monitor.
-print(screenWidth, screenHeight)
+# screenWidth, screenHeight = pyautogui.size() # Get the size of the primary monitor.
+# print(screenWidth, screenHeight)
 
 def click_escolher_arquivo():
-  X=2022
-  Y=345
+  # X=2022
+  # Y=345
+  X=2092
+  Y=374
   pyautogui.click(X, Y)
+  pyautogui.sleep(4)
 
 def click_selecionar_pasta():
-  X=2015
-  Y=556
+  # X=2015
+  # Y=556
+  X=1980
+  Y=405
   pyautogui.click(X, Y)
 
+  pyautogui.sleep(3)
+  pyautogui.press('enter')
+
+
+
+  # X=2243
+  # Y=143
+  # pyautogui.doubleClick(X, Y)
+  # # doubleClick
+  # pyautogui.sleep(3)
+  # ## send arquivo
+  ## select arquivo
+
+
+  # X=2838
+  # Y=36
+  # pyautogui.click(X, Y)
+
 def click_verificar_arquivo():
-  X=2683
-  Y=575
+  # X=2683
+  # Y=575
+  X=2680
+  Y=608
   pyautogui.click(X, Y)
   ## await 60seconds
-  pyautogui.sleep(60)
+  pyautogui.sleep(15)
 
 def click_lower_folder():
   X=3559
@@ -25,62 +50,70 @@ def click_lower_folder():
   pyautogui.click(X, Y)
 
 def click_first_file_lower_folder():
-  X=3284
-  Y=630
+  # X=3284
+  # Y=630
+
+  X=3489
+  Y=657
   pyautogui.click(X, Y)
+  pyautogui.sleep(2)
 
 def click_upper_folder():
-  X=3550
-  Y=28
+  # X=3550
+  # Y=28
+  X=3596
+  Y=32
   pyautogui.click(X, Y)
-
+  pyautogui.sleep(2)
 
 def change_tipo_de_arquivo():
-  X=2536
-  Y=418
+  X=2526
+  Y=442
   pyautogui.click(X, Y)
   ## wait 3seconds
-  pyautogui.sleep(3)
-  X=2480
-  Y=505
+  pyautogui.sleep(2)
+  X=2492
+  Y=531
+
   pyautogui.click(X, Y)
+  pyautogui.sleep(2)
+
+def get_mouse_position():
+    input("Click anywhere on the screen...")
+    #   # Get mouse click position
+    click_x, click_y = pyautogui.position()
+    print(f"Mouse clicked at: X={click_x}, Y={click_y}")
+
 
 def main():
     # Wait for user to click
     while True:
-      # input("Click anywhere on the screen...")
 
+      # get_mouse_position()
 
-      #   # Get mouse click position
-      # click_x, click_y = pyautogui.position()
-
-
-      # print(f"Mouse clicked at: X={click_x}, Y={click_y}")
 
       click_escolher_arquivo()
-      ## wait 1seconds
-      pyautogui.sleep(1)
+
       click_selecionar_pasta()
-      ## wait 1seconds
+
       pyautogui.sleep(2)
-      ## send key enter
-      pyautogui.press('enter')
-      ## wait 5 seconds
-      pyautogui.sleep(3)
+
+
       change_tipo_de_arquivo()
+
       click_verificar_arquivo()
 
-      # press control+X
-
+      # # press control+X
       click_first_file_lower_folder()
       pyautogui.hotkey('ctrl', 'x')
-      # # wait 5 seconds
-      pyautogui.sleep(3)
+
       click_upper_folder()
       pyautogui.hotkey('ctrl', 'v')
 
-      ## wait 7 minutes
-      pyautogui.sleep(300)
+
+      # ## wait 4 minutes to upload next file
+      # pyautogui.sleep(240)
+      pyautogui.sleep(200)
 
 
 if __name__ == "__main__":
